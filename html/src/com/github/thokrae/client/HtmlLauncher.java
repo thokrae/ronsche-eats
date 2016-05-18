527508976a2acc5389c4b5793ac9e246d21c5dd4
Thomas Krämer <thomas.kraemer@posteo.de>
2016-05-18 22:22:16 +0200

    created libgdx project

---
+++ html/src/com/github/thokrae/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.github.thokrae.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.thokrae.RonscheEatsMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new RonscheEatsMain();
+        }
+}
